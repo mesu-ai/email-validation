6b14c0d2987dbf7e49450b192fda1f85ec7836ce
mesu-ai <mesu.s.ss@gmail.com>
2021-09-11 05:57:47 +0600

    email validation

--- email.js
+++ email.js
@@ -6,17 +6,34 @@ document.getElementById('email-submit').addEventListener('click', function () {
     const valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
     if (!email) {
-        console.log('Enter Email First !');
+        //  console.log('Enter Email First !');
+        statusMsg('Enter Email First!', 'red');
     } else if (email) {
         if (valid.test(email)) {
-            console.log('Valid Email');
+            // console.log('Valid Email');
+            statusMsg('Valied Email!', 'green');
 
         }
         else {
-            console.log('Invalid Email');
+            // console.log('Invalid Email');
+            statusMsg('Invalied Email!', 'red');
         }
 
     }
 
 
 });
+
+const statusMsg = (massage, color) => {
+
+    const div = document.getElementById('status-msg');
+    div.textContent = '';
+
+    const h5 = document.createElement('h5');
+
+    h5.innerText = massage;
+    h5.style.color = color;
+
+    div.appendChild(h5);
+
+}
